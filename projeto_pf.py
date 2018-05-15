d6964ed60b8a4b44e1f44bfe9572c15fc1cef434
hugoecarl <mormeme@gmail.com>
2018-05-14 21:59:33 -0300

    Projeto Finalizado

--- projeto_pf.py
+++ projeto_pf.py
@@ -4,10 +4,11 @@
 Esta classe deve conter todas as suas implementações relevantes para seu filtro de partículas
 """
 
-from pf import Particle, create_particles
+from pf import Particle, create_particles, draw_random_sample
 import numpy as np
 import inspercles # necessário para o a função nb_lidar que simula o laser
 import math
+from scipy.stats import norm
 
 
 largura = 775 # largura do mapa
@@ -19,7 +20,7 @@ robot = Particle(largura/2, altura/2, math.pi/4, 1.0)
 # Nuvem de particulas
 particulas = []
 
-num_particulas = 150
+num_particulas = 1000
 
 
 # Os angulos em que o robo simulado vai ter sensores
@@ -38,7 +39,23 @@ movimentos_longos = [[-10, -10, 0], [-10, 10, 0], [-10,0,0], [-10, 0, 0],
 # Lista curta
 movimentos_curtos = [[-10, -10, 0], [-10, 10, 0], [-10,0,0], [-10, 0, 0]]
 
-movimentos = movimentos_longos
+movimentos_relativos = [[0, -math.pi/3],[10, 0],[10, 0], [10, 0], [10, 0],[15, 0],[15, 0],[15, 0],[0, -math.pi/2],[10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [0, -math.pi/2], 
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [0, -math.pi/2], 
+                       [10,0], [0, -math.pi/4], [10,0], [10,0], [10,0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0]]
+
+
+
+movimentos = movimentos_relativos
 
 
 
@@ -46,17 +63,22 @@ def cria_particulas(minx=0, miny=0, maxx=largura, maxy=altura, n_particulas=num_
     """
         Cria uma lista de partículas distribuídas de forma uniforme entre minx, miny, maxx e maxy
     """
-    return create_particles(robot.pose(), maxx/2 , maxy/2, math.pi, num_particulas)
+    return create_particles(robot.pose(), maxx/2, maxy/2,math.pi, n_particulas)
     
 def move_particulas(particulas, movimento):
     """
-        Recebe um movimento na forma [x,y, theta]  e o aplica a todas as partículas
+        Recebe um movimento na forma [deslocamento, theta]  e o aplica a todas as partículas
         Assumindo um desvio padrão para cada um dos valores
         Esta função não precisa devolver nada, e sim alterar as partículas recebidas.
+        
+        Sugestão: aplicar move_relative(movimento) a cada partícula
         
         Você não precisa mover o robô. O código fornecido pelos professores fará isso
         
     """
+    for i in particulas:
+      i.move_relative(norm.rvs(movimento,0.2))
+    
     return particulas
     
 def leituras_laser_evidencias(robot, particulas):
@@ -76,6 +98,22 @@ def leituras_laser_evidencias(robot, particulas):
     leitura_robo = inspercles.nb_lidar(robot, angles)
     
     # Voce vai precisar calcular a leitura para cada particula usando inspercles.nb_lidar e depois atualizar as probabilidades
+    lista = []
+    for i in particulas:
+      leitura_particulas = inspercles.nb_lidar(i, angles)
+      P = 0
+      for h in leitura_particulas:
+        leitura = leitura_particulas[h]
+        prob = norm.pdf(leitura, loc = leitura_robo[h], scale = 7)
+        P += prob
+
+        if P == 0.0:
+          P = 0.00000000000000000000000000001
+    
+      lista.append(P)
+    
+    for j in range(len(particulas)):
+    	particulas[j].w = lista[j]*(1/sum(lista))
 
 
     
@@ -91,6 +129,16 @@ def reamostrar(particulas, n_particulas = num_particulas):
         
         Use 1/n ou 1, não importa desde que seja a mesma
     """
+    pesos = []
+    for l in particulas:
+      pesos.append(l.w)
+    print(pesos)
+    particulas = draw_random_sample(particulas, pesos, num_particulas)
+    for p in particulas:
+    	p.x = norm.rvs(p.x,10)
+    	p.y = norm.rvs(p.y,10)
+    	p.theta = norm.rvs(p.theta,math.pi/12)
+    	p.w = 1 
     return particulas
 
 
